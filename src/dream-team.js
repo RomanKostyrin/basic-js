424665b760db75ac5ab52aab9ef7a84ef47e5663
roman kostyrin <enemy-iubip@mail.ru>
2021-09-08 12:45:12 +0300

    init: add dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,19 +1,31 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from '../extensions/index.js'
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false
+  }
+  let dt = []
+
+  members.forEach((el) => {
+    if (typeof el === 'string') {
+      dt.push(el.replace(/\s/g, ''))
+    }
+  })
+  dt = dt.map((el) => el[0])
+  dt = dt.map((el) => el.toUpperCase())
+  dt.sort((a, b) => a[0].localeCompare(b[0]))
+  return dt.join('')
 }
