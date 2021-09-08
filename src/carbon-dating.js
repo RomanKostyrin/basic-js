c0fd2254025e90d5568259722cb3e5dd2e98e920
roman kostyrin <enemy-iubip@mail.ru>
2021-09-08 12:16:08 +0300

    init: add carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,23 +1,35 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from '../extensions/index.js'
 
-const MODERN_ACTIVITY = 15;
-const HALF_LIFE_PERIOD = 5730;
+const MODERN_ACTIVITY = 15
+const HALF_LIFE_PERIOD = 5730
 
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(last_activity) {
+  if (
+    last_activity >= 15 ||
+    last_activity <= 0 ||
+    typeof last_activity !== 'string'
+  ) {
+    return false
+  }
+  let valid_activity = parseFloat(+last_activity)
+  let res =
+    Math.log(MODERN_ACTIVITY / valid_activity) / (0.693 / HALF_LIFE_PERIOD)
+  if (Number.isNaN(res)) {
+    return false
+  }
+  return Math.ceil(res)
 }
