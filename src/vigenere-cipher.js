6f6db2f597885fae959f6e47f4ad9b5a74e2b25d
roman kostyrin <enemy-iubip@mail.ru>
2021-09-13 23:47:11 +0300

    feat: make half vigenere cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,31 +1,66 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from '../extensions/index.js'
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(is) {
+    this.reverse = true | is
+    this.lang = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (!key || !message) {
+      throw new Error('Incorrect arguments!')
+    }
+
+    key = key.toUpperCase()
+    message = message.toUpperCase()
+    let encryptMessage = ''
+    const length = message.split(' ').reduce((acc, el) => acc + el.length, 0)
+
+    if (key.length < length) {
+      key = key.repeat(5).slice(0, length)
+    }
+    let shift = 0
+    for (let i = 0; i < length; i++) {
+      if (message[i + shift] === ' ') {
+        shift++
+        encryptMessage += ' '
+      }
+      if (this.lang.indexOf(message[i + shift]) === -1) {
+        encryptMessage += message[i + shift]
+      } else {
+        const char =
+          (this.lang.indexOf(message[i + shift]) + this.lang.indexOf(key[i])) %
+          26
+        encryptMessage += this.lang[char]
+      }
+    }
+    if (!this.reverse) {
+      encryptMessage = encryptMessage.reverse()
+    }
+    return encryptMessage
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!key || !encryptedMessage) {
+      throw new Error('Incorrect arguments!')
+    }
   }
 }
