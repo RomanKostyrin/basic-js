2e9af3bce58669a92dddfdddc0043da62d0b4972
roman kostyrin <enemy-iubip@mail.ru>
2021-09-13 17:37:00 +0300

    feat: add st- tasks

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -1,4 +1,4 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from '../extensions/index.js'
 
 /**
  * Given a string, return its encoding version.
@@ -10,7 +10,23 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  let decoder = str.split('')
+  for (let i = 1; i < str.length; i++) {
+    if (str[i] === str[i - 1]) {
+      decoder[i - 1] = 2
+    }
+  }
+  let decodeStr = ''
+
+  decoder.forEach((el, index) => {
+    let elem = el
+    if (typeof el === 'number' && typeof decoder[index - 1] === 'number') {
+      elem = +decodeStr[decodeStr.length - 1]
+      elem++
+      decodeStr = decodeStr.slice(0, decodeStr.length - 1)
+    }
+    decodeStr = decodeStr + elem.toString()
+  })
+  return decodeStr
 }
